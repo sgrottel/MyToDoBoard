ad4e04f5ee8a001dd7ce681100f2efd12808a98b
SGrottel <sgrottel@sgrottel.de>
2022-12-28 11:14:57 +0100

    Fundamental board layout

---
+++ WpfApp/Board/Card.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToDoBoard.Board
+{
+
+	public class Card : INotifyPropertyChanged
+	{
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		private string title = string.Empty;
+
+		/*
+		  - Title & Icon
+		  - Labels
+		  - Due Date (for displaying purpose only)
+		  - Description
+		  - Checklist
+		  - List of Links
+		  - List of other Cards this one depends on
+		  - Comments
+		 */
+
+		public string Title
+		{
+			get => title;
+			set
+			{
+				if (title != value)
+				{
+					title = value;
+					PropertyChanged?.Invoke(this, new(nameof(Title)));
+				}
+			}
+		}
+
+	}
+
+}
---
+++ WpfApp/Board/Column.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToDoBoard.Board
+{
+
+	public class Column : INotifyPropertyChanged
+	{
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		private string title = string.Empty;
+
+		public string Title
+		{
+			get => title;
+			set
+			{
+				if (title != value)
+				{
+					title = value;
+					PropertyChanged?.Invoke(this, new(nameof(Title)));
+				}
+			}
+		}
+
+		private Card[] cards = Array.Empty<Card>();
+		public Card[] Cards
+		{
+			get => cards;
+			set
+			{
+				if (cards != value)
+				{
+					cards = value;
+					PropertyChanged?.Invoke(this, new(nameof(Cards)));
+				}
+			}
+		}
+
+	}
+
+}
